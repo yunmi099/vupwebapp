256193e9aececcd00823a259caf5718e8758de38
yunmi099 <yunmi099@ajou.ac.kr>
2023-02-16 17:01:40 +0900

    chart ui 수정

--- src/components/chart/option.js
+++ src/components/chart/option.js
@@ -5,11 +5,7 @@ export const options_1 = {
   interaction: {
     mode: "index",
   },
-  elements: {
-    point: {
-      radius: 0.5,
-    },
-  },
+
   plugins: {
     legend: {
       labels: {
@@ -21,7 +17,11 @@ export const options_1 = {
       },
     },
   },
-
+  elements: {
+    point: {
+      radius: 0.9,
+    },
+  },
   scales: {
     y: {
       // 'y'라는 id를 가진 y축에 대한 설정
@@ -85,16 +85,16 @@ export const options_1 = {
   },
 };
 export const options_2 = {
+  elements: {
+    point: {
+      radius: 0.9,
+    },
+  },
   responsive: true,
   spanGaps: true,
   grouped: true,
   interaction: {
     mode: "index",
-  },
-  elements: {
-    point: {
-      radius: 0.5,
-    },
   },
   plugins: {
     legend: {
@@ -117,6 +117,7 @@ export const options_2 = {
         // 가로선 설정
         color: "#E2E2E230",
       },
+
       afterDataLimits: (scale) => {
         // y축의 최대값은 데이터의 최대값에 딱 맞춰져서 그려지므로
         // y축 위쪽 여유공간이 없어 좀 답답한 느낌이 들 수 있는데요,
@@ -125,7 +126,8 @@ export const options_2 = {
       },
       axis: "y", // 이 축이 y축임을 명시해줍니다.
       display: true, // 축의 가시성 여부도 설정할 수 있습니다.
-      position: "left", // 축이 왼쪽에 표시될지, 오른쪽에 표시될지 정할 수 있습니다.
+      position: "left",
+      // 축이 왼쪽에 표시될지, 오른쪽에 표시될지 정할 수 있습니다.
       title: {
         // 이 축의 단위 또는 이름도 title 속성을 이용하여 표시할 수 있습니다.
         display: true,
@@ -146,11 +148,13 @@ export const options_2 = {
         display: true,
         align: "end",
         color: "#808080",
+
         font: {
           size: 12,
           family: "'Noto Sans KR', sans-serif",
           weight: 300,
         },
+
         text: "전력",
       },
     },
